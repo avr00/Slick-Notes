db864a6547d4868eb2af4caa0270f74c1eb358a0
AVR <avrivadeneira1@gmail.com>
2019-08-07 17:23:03 -0400

    add create post mutation

--- components/posts/NewPost.js
+++ components/posts/NewPost.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { gql } from "apollo-boost";
+import { useMutation } from "@apollo/react-hooks";
 
 import PostForm from "./PostForm";
 
 const NewPost = () => {
-  const onSubmit = ({ title, body }) => {
+  const [createPost, { data }] = useMutation(NEW_POST);
+  const onSubmit = async ({ title, body }) => {
     console.log("TITLE AND BODY", title, body);
+    try {
+      await createPost({ variables: { title, body } });
+      console.log("data", data);
+    } catch (err) {
+      console.error(err);
+    }
   };
   return (
     <View>
@@ -13,5 +22,15 @@ const NewPost = () => {
     </View>
   );
 };
+
+const NEW_POST = gql`
+  mutation newPost($title: String!, $body: String) {
+    createPost(title: $title, body: $body) {
+      id
+      title
+      body
+    }
+  }
+`;
 
 export default NewPost;
--- components/posts/PostForm.js
+++ components/posts/PostForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, Button } from "react-native";
+import { View, StyleSheet, TextInput, Button } from "react-native";
 
 const PostForm = props => {
   const [title, setTitle] = useState("");
@@ -13,11 +13,33 @@ const PostForm = props => {
   };
   return (
     <View>
-      <TextInput onChangeText={value => setTitle(value)} value={title} />
-      <TextInput onChangeText={value => setBody(value)} value={body} />
+      <TextInput
+        style={styles.title}
+        onChangeText={value => setTitle(value)}
+        value={title}
+      />
+      <TextInput
+        style={styles.body}
+        onChangeText={value => setBody(value)}
+        value={body}
+      />
       <Button title="Save Post" onPress={submitForm} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  title: {
+    height: 40,
+    borderColor: "#333",
+    borderWidth: 1
+  },
+  body: {
+    height: 400,
+    borderColor: "#333",
+    borderWidth: 1,
+    textAlignVertical: "top"
+  }
+});
 
 export default PostForm;
