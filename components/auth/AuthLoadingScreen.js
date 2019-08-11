4c8af7a7516833e6d3cb2f5158c57d79dc4ab867
AVR <avrivadeneira1@gmail.com>
2019-08-10 23:36:04 -0400

    styled authloading

--- components/auth/AuthLoadingScreen.js
+++ components/auth/AuthLoadingScreen.js
@@ -1,7 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { ActivityIndicator, StatusBar, StyleSheet, View } from "react-native";
+import { Spinner } from "native-base";
 import { useQuery } from "@apollo/react-hooks";
 import { USER } from "../../graphql/queries";
+import styled from "styled-components";
+
+const AuthLoadingContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  background-color: #272727;
+`;
 
 const AuthLoadingScreen = props => {
   const { loading, error, data } = useQuery(USER, {
@@ -13,10 +21,10 @@ const AuthLoadingScreen = props => {
 
   // Render any loading content that you like here
   return (
-    <View>
-      <ActivityIndicator />
+    <AuthLoadingContainer>
       <StatusBar barStyle="default" />
-    </View>
+      <Spinner color="blue" />
+    </AuthLoadingContainer>
   );
 };
 
